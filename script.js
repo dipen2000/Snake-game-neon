1ada1c230fa1c815853cc8488e4de1ec6525baf1
dipen2000 <dipenchavda2000@gmail.com>
2021-08-11 01:14:21 +0530

    Style : Made the gameboard , snake and the food

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-let lastRenderTime = 0;
-let SNAKE_SPEED = 2;
-function main(currentTime){
-    requestAnimationFrame(main);
-    let timeBetweenTwoPaints = (currentTime - lastRenderTime)/1000;
-    if(timeBetweenTwoPaints < 1 / SNAKE_SPEED){
-       return;
-    }
-    console.log("paint");
-    lastRenderTime = currentTime;
+// let lastRenderTime = 0;
+// let SNAKE_SPEED = 2;
+// function main(currentTime){
+//     requestAnimationFrame(main);
+//     let timeBetweenTwoPaints = (currentTime - lastRenderTime)/1000;
+//     if(timeBetweenTwoPaints < 1 / SNAKE_SPEED){
+//        return;
+//     }
+//     console.log("paint");
+//     lastRenderTime = currentTime;
+// }
+
+// requestAnimationFrame(main);
+
+const gameboard = document.getElementById('gameboard');
+
+
+for(let i=0;i<5;i++){
+    const snake = document.createElement("div");
+    snake.classList.add("snake");
+    gameboard.appendChild(snake);
+    snake.style.gridColumnStart = 11;
+    snake.style.gridRowStart = 11+i;
 }
 
-requestAnimationFrame(main);
+
+const food = document.createElement("div");
+food.classList.add("food");
+gameboard.appendChild(food);
+food.style.gridColumnStart = 11;
+food.style.gridRowStart = 8;
+
