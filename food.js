b0f8a554eb21c6d3733466bd12a55775839e3253
dipen2000 <dipenchavda2000@gmail.com>
2021-08-11 10:49:08 +0530

    feat : when snake eats the food gets to a random space and will never be on top of the snake

--- food.js
+++ food.js
@@ -1,4 +1,6 @@
 import {snakeBody  , onSnake} from "./snake.js"
+import {getRandomPosition} from "./grid.js"
+
 let food = {x:0 , y:0};
 food={x:20 , y:2};
 export function updateFood(){
@@ -6,7 +8,8 @@ export function updateFood(){
     if(onSnake(food)){
         // expandSnake();
         
-        food = {x : 2 , y : 20};
+        food = getRandomPositionNotOnTheSnake();
+        console.log(food);
     }
 }
 
@@ -17,4 +20,12 @@ export function drawFood(gameboard){
     gameboard.appendChild(foodElement);
     foodElement.style.gridRowStart = food.y;
     foodElement.style.gridColumnStart = food.x;
+}
+
+function getRandomPositionNotOnTheSnake(){
+    let newPosition;
+    while(newPosition == null || onSnake(getRandomPosition())){
+        newPosition = getRandomPosition();
+    }
+    return newPosition;
 }
--- grid.js
+++ grid.js
@@ -1,3 +1,10 @@
+let randomBlock = {x:0 , y:0};
 export function equalPosition(pos1 , pos2){
     return pos1.x === pos2.x && pos1.y === pos2.y;
+}
+
+export function getRandomPosition(){
+    const x_coordinate = Math.floor(Math.random()*21)+1;
+    const y_coordinate = Math.floor(Math.random()*21)+1;
+    return {x : x_coordinate , y : y_coordinate};
 }
